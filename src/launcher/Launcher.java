5bd61f982f6e8c43b2226a1cd3c64aebf65295a3
Kwarthys <Kwarthys@gmail.com>
2017-11-12 18:42:53 +0100

    They see me tryiiin'

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,10 +1,11 @@
 package launcher;
 
+import windows.Window;
+
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Window();
 	}
 
 }
